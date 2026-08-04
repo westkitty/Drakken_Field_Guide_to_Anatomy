23d6d33424fa36dfce02a1b4ba4310c1534dbea6
westkitty <andrew.dolby@westcat.ca>
2026-08-04 02:05:33 -0400

    polish: activate fourth-wave stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './polish-wave2.css';
 import './polish-wave2-repairs.css';
 import './polish-wave3.css';
 import './polish-wave3-repairs.css';
+import './polish-wave4.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
